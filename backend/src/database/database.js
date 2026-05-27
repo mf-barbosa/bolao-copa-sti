a60d1b6752918f84a24c31c5e03fa15b0d8bbdc8
mf-barbosa <matheus_gordo14@hotmail.com>
2026-05-27 13:43:30 -0300

    chore: configura caminho do banco por ambiente

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,13 +1,29 @@
+require("dotenv").config();
+
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-const dbPath = path.resolve(__dirname, "../../database.sqlite");
+function resolveDatabasePath() {
+  const configuredPath = process.env.DATABASE_PATH;
+
+  if (!configuredPath) {
+    return path.resolve(__dirname, "../../database.sqlite");
+  }
+
+  if (path.isAbsolute(configuredPath)) {
+    return configuredPath;
+  }
+
+  return path.resolve(process.cwd(), configuredPath);
+}
+
+const dbPath = resolveDatabasePath();
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Erro ao conectar no banco:", err);
   } else {
-    console.log("Conectado ao banco SQLite!");
+    console.log(`Conectado ao banco SQLite em: ${dbPath}`);
   }
 });
 
